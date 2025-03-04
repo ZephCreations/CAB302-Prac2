635b7d41dd60fc5807d57236228ae2688369b550
ZephCreations <STEM-futur3@outlook.com>
2025-03-04 12:10:16 +1000

    Updated Authentication System

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+
 public class Main {
     // Static list of users, acting as a database
     private static ArrayList<User> users = new ArrayList<>();
 
     // Mock authentication service that always returns the first user when log in, and does nothing when sign up
-    private static IAuthenticationService authService = new IAuthenticationService() {
+    private static IAuthenticationService authService = new AuthenticationService(users) {
         @Override
         public User signUp(String username, String password) {
             return null;
@@ -87,7 +89,15 @@ public class Main {
         System.out.print("Enter your password: ");
         String password = scanner.nextLine();
         User user = authService.signUp(username, password);
-        // TODO Later: Shows a message based on the result
+        // TODO Now: Show a message based on the result of the signUp method:
+        // - If the user is not null, show "User <username> has been created successfully!"
+        // - If the user is null, show "The username is already taken!"
+        if (user != null) {
+            System.out.println("User " + user.getUsername() + " had been created successfully!");
+        } else {
+            System.out.println("The username is already taken!");
+        }
+
     }
 
     /**
